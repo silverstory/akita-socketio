5ac48119f250178716cd11cadce1ac3161072344
Eprel Acer ICTO <apr.medina@gmail.com>
2019-11-13 16:30:25 +0800

    Added reactive logs

--- server/app.js
+++ server/app.js
@@ -16,9 +16,12 @@ let gate6 = 0;
 let gate4 = 0;
 let gate2 = 0;
 
-let logs = [];
+let entirelist = [];
 
 io.on('connection', function (socket) {
+
+  // livefeed
+
   socket.emit('list', {
     type: 'SET',
     data: list
@@ -70,6 +73,65 @@ io.on('connection', function (socket) {
       data: list.find(current => current.id === id)
     });
   })
+
+  // end livefeed
+
+  // entirelist
+
+  socket.emit('entirelist', {
+    type: 'SET',
+    data: entirelist
+  });
+
+  // socket.on('entirelist:feed', item => {
+
+  //   // limits list items to only 4 items
+  //   const rightmostlist = entirelist.slice(0,3);
+  //   entirelist = [item, ...rightmostlist];
+
+  //   io.sockets.emit('entirelist', {
+  //     type: 'SET',
+  //     data: entirelist
+  //   });
+  // });
+
+  socket.on('entirelist:add', item => {
+    entirelist.push(item);
+    io.sockets.emit('entirelist', {
+      type: 'ADD',
+      data: item
+    });
+  });
+
+  socket.on('entirelist:remove', id => {
+    entirelist = entirelist.filter(item => item.id !== id);
+
+    io.sockets.emit('entirelist', {
+      type: 'REMOVE',
+      ids: id
+    });
+  });
+
+  socket.on('entirelist:toggle', id => {
+    entirelist = entirelist.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed
+        }
+      }
+      return item;
+    });
+
+    io.sockets.emit('entirelist', {
+      type: 'UPDATE',
+      ids: id,
+      data: entirelist.find(current => current.id === id)
+    });
+  })
+
+  // end entirelist
+
 });
 
 server.listen(8000);
